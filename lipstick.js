03bd24bbbaccde868d7f7f9852bfddda4b37de2a
Sanskriti-create <82824454+Sanskriti-create@users.noreply.github.com>
2021-12-29 19:00:35 +0530

    Add files via upload

---
+++ lipstick.js
@@ -0,0 +1,22 @@
+function preload()
+{
+    
+}
+
+
+function setup()
+{
+    canvas = createCanvas(450, 400);
+    canvas.center();
+}
+
+
+function draw()
+{
+
+}
+
+function back()
+{
+    window.location = "index.html";
+}
